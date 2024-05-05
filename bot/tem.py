7c1fc9f907265082c43309d3d24d484aa3290e80
Alex <aablagait@MacBook-Air-Alex.local>
2024-05-05 21:10:34 +0300

    релиз 5.05

---
+++ bot/tem.py
@@ -0,0 +1,29 @@
+# Содержание update.message
+print({'text': '/start',
+       'entities': [{'offset': 0,
+                     'type': 'bot_command',
+                     'length': 6}],
+       'photo': [],
+       'caption_entities': [],
+       'chat': {'username': 'aablagait',
+                'type': 'private',
+                'first_name': 'Алексей',
+                'id': 385912395,
+                'last_name': 'Викторов'},
+       'channel_chat_created': False,
+       'supergroup_chat_created': False,
+       'group_chat_created': False,
+       'new_chat_members': [],
+       'message_id': 218,
+       'delete_chat_photo': False,
+       'new_chat_photo': [],
+       'date': 1714755226,
+       'from': {'is_bot': False,
+                'id': 385912395,
+                'username': 'aablagait',
+                'language_code': 'ru',
+                'last_name': 'Викторов',
+                'first_name': 'Алексей'}
+       }
+      )
+
